488b78de37a1f1b0698ac64bdc33483a692feba0
Kysela <bythedroid@gmail.com>
2020-10-04 17:47:49 +0200

    ln -sf

--- recovery/root/sbin/unified-script.sh
+++ recovery/root/sbin/unified-script.sh
@@ -60,4 +60,8 @@ case $project in
         ;;
 esac
 
+ln -sf /dev/block/mapper/system /dev/block/bootdevice/by-name/system
+ln -sf /dev/block/mapper/vendor /dev/block/bootdevice/by-name/vendor
+ln -sf /dev/block/mapper/product /dev/block/bootdevice/by-name/product
+
 exit 0
