ce67351591d021c9969bb5a9bd8572af9db870d2
Kysela <bythedroid@gmail.com>
2020-10-07 20:08:04 +0200

    enable recovery.perf.mode

--- init/Android.mk
+++ init/Android.mk
@@ -13,17 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := \
-system/core/init \
-system/core/base/include
-
-LOCAL_STATIC_LIBRARIES := libbase
+    system/core/base/include \
+    system/core/init
+    
 LOCAL_SRC_FILES := init_curtana.cpp
 LOCAL_MODULE := libinit_curtana
 
